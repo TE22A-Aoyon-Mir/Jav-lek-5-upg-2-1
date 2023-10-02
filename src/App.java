d8f41fa8ef280c2f9e3c3ac1775ceb38ae1bb9e0
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-02 08:39:14 +0200

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner tangentbord = new Scanner (System.in);
+
+        //a
+        int[] fält1 = new int[4];
+        System.out.println("Ange elemnt 0: " );
+        fält1[0] = tangentbord.nextInt();
+        System.out.println("Ange elemnt 1: " );
+        fält1[1] = tangentbord.nextInt();
+        System.out.println("Ange elemnt 2: " );
+        fält1[2] = tangentbord.nextInt();
+        System.out.println("Ange elemnt 3: " );
+        fält1[3] = tangentbord.nextInt();
+        System.out.println("Element 0: "+fält1[0]);
+        System.out.println("Element 1: "+fält1[1]);
+        System.out.println("Element 2: "+fält1[2]);
+        System.out.println("Element 3: "+fält1[3]);
+
+        int plats = fält1[0];
+        fält1[0] = fält1[3];
+        fält1[3] = plats;
+        int plats2 = fält1[1];
+        fält1[1] = fält1[2];
+        fält1[2] = plats2;
+        System.out.println("Med bytt plats: ");
+        System.out.println("Element 0: "+fält1[0]);
+        System.out.println("Element 1: "+fält1[1]);
+        System.out.println("Element 2: "+fält1[2]);
+        System.out.println("Element 3: "+fält1[3]);
     }
 }
