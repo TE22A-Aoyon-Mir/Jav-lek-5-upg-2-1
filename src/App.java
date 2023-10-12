fe1e7e77d62b61c4999183323ab0622e046cabe5
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-12 10:10:47 +0200

    Gjort klar lek 5 uppgift 2

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         
         Scanner tangentbord = new Scanner (System.in);
 
-        //a
+        System.out.println("Del a");
         int[] fält1 = new int[4];
         System.out.println("Ange elemnt 0: " );
         fält1[0] = tangentbord.nextInt();
@@ -31,5 +31,20 @@ public class App {
         System.out.println("Element 1: "+fält1[1]);
         System.out.println("Element 2: "+fält1[2]);
         System.out.println("Element 3: "+fält1[3]);
+
+        tangentbord.nextLine();
+        System.out.println("Del b");
+        String[] fält2 = new String[3];
+        fält2[0] = "hej";
+        fält2[1] = "på";
+        fält2[2] = "dig";
+        System.out.println("Gissa ett ord: ");
+        String gi = tangentbord.nextLine();
+        System.out.println("Ord 0");
+        System.out.println(gi.equalsIgnoreCase(fält2[0]));
+        System.out.println("Ord 1");
+        System.out.println(gi.equalsIgnoreCase(fält2[1]));
+        System.out.println("Ord 2");
+        System.out.println(gi.equalsIgnoreCase(fält2[2]));
     }
 }
